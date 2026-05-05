06f86698eb51ee364fa6a5fe5b2c5ea8407fe966
juanrmv528e <juanramboy1@gmail.com>
2026-05-04 20:28:06 -0500

    16

--- car_utils.py
+++ car_utils.py
@@ -1,6 +1,6 @@
 # FREEZE CODE BEGIN
-from car import *
-
+from car import Car
+ 
 def create_car_from_input():
     car_id = input("Enter car ID (e.g., CAR001):\n")
     brand = input("Enter car brand:\n")
@@ -8,8 +8,8 @@ def create_car_from_input():
     color = input("Enter car color:\n")
     mileage = float(input("Enter mileage:\n"))
     return Car(car_id, brand, year, color, mileage)
-
+ 
 def display_cars(car_dict):
-  for car in car_dict.values():
-            print(car)
+    for car in car_dict.values():
+        print(car)
 # FREEZE CODE END
