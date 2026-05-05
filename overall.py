2e0cdfc0a540271f512685f45442d8bc5f27f2ec
juanrmv528e <juanramboy1@gmail.com>
2026-05-04 20:27:42 -0500

    15

--- overall.py
+++ overall.py
@@ -1,44 +1,37 @@
-# TODO: Import modules
-def main():
-    cars = {}  # Dictionary to store cars with car_id as key and car objects as values
-
-    while True:
-        print("\nMenu:")
-        print("1. Add a new car")
-        print("2. View all cars")
-        print("3. Drive a car")
-        print("4. Paint a car")
-        print("5. Exit")
-
-        choice = input("Choose an option:\n")
-
-        if choice == '1':
-          """TODO: Call the appropriate function from utils.py to create 
-          the car, add it to the dictionary, and print the car."""
-
-
-        elif choice == '2':
-          """TODO: Call the appropriate function from utils.py to display
-          all the cars in the cars dictionary."""
-
-        elif choice == '3':
-          car_id = input("Enter the car ID to drive:\n")
-          miles = float(input("How many miles to drive?\n"))
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to increase the mileage of the car, and print the car."""
-          
-        elif choice == '4':
-          car_id = input("Enter the car ID to paint:\n")
-          new_color = input("Enter the new color:\n")
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to change the color of the car, and print the car."""
-
-        elif choice == '5':
-            print("Goodbye!")
-            break
-
-        else:
-            print("Invalid option. Please try again.")
-
-if __name__ == "__main__":
-    main()
+from car_utils import create_car_from_input, display_cars
+ 
+cars = {}
+ 
+while True:
+    print("Options: (1) Add Car, (2) View Cars, (3) Drive Car, (4) Paint Car, (5) Quit:")
+    option = input()
+ 
+    if option == '1':
+        new_car = create_car_from_input()
+        cars[new_car.car_id] = new_car
+        print(new_car)
+        print("Car added.")
+ 
+    elif option == '2':
+        display_cars(cars)
+ 
+    elif option == '3':
+        car_id = input("Enter car ID:\n")
+        miles = float(input("Enter miles driven:\n"))
+        cars[car_id].drive(miles)
+        print("Mileage updated.")
+        print(cars[car_id])
+ 
+    elif option == '4':
+        car_id = input("Enter car ID:\n")
+        new_color = input("Enter new color:\n")
+        cars[car_id].change_color(new_color)
+        print("Color updated.")
+        print(cars[car_id])
+ 
+    elif option == '5':
+        print("Goodbye!")
+        break
+ 
+    else:
+        print("Invalid option selected!")
