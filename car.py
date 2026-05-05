326a4af0e1cd06a3c49cb78307076a2a57de268c
juanrmv528e <juanramboy1@gmail.com>
2026-05-04 20:28:19 -0500

    17

--- car.py
+++ car.py
@@ -1,2 +1,14 @@
 # Write your class definition here!
-
+from car import Car
+ 
+def create_car_from_input():
+    car_id = input("Enter car ID (e.g., CAR001):\n")
+    brand = input("Enter car brand:\n")
+    year = int(input("Enter car year:\n"))
+    color = input("Enter car color:\n")
+    mileage = float(input("Enter mileage:\n"))
+    return Car(car_id, brand, year, color, mileage)
+ 
+def display_cars(car_dict):
+    for car in car_dict.values():
+        print(car)
